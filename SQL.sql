c87330c163a384bb3d9db67b1ec52d2f3e2995eb
IariIcaro <30503924+IariIcaro@users.noreply.github.com>
2017-07-31 19:27:21 +0200

    SQL

---
+++ SQL.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE IF NOT EXISTS 'Gaming';
+
+USE 'Gaming';
+
+/*UTENTE*/
+DROP TABLE IF EXISTS 'utente'
+CREATE TABLE 'utente'(
+'id' int(5) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+'nome' varchar(20) NOT NULL,
+'cognome' varchar(20) NOT NULL,
+'username' varchar(10) NOT NULL UNIQUE,
+'email' varchar(20) NOT NULL UNIQUE,
+'password' varchar(30) NOT NULL,
+'tipo' ENUM('U','M') NOT NULL DEFAULT 'U',
+'livello' int(33) NOT NULL DEFAULT '0'
+)ENGINE=innoDB;
+
+
+/*RECENSIONE*/
+DROP TABLE IF EXISTS 'recensione'
+CREATE TABLE 'recensione'(
+'id' int(5) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+'approvazione' tinyint(1) NOT NULL DEFAULT '0',
+'testo' varchar(255) NOT NULL,
+'gioco' int(5) NOT NULL REFERENCES gioco ('id'),
+'utente' int(5) NOT NULL REFERENCES utente ('id'),
+UNIQUE ('gioco', 'utente'),
+)ENGINE=innoDB;
+
+
+
+/*GIOCO*/
+DROP TABLE IF EXISTS 'gioco'
+CREATE TABLE 'gioco'(
+'id' int(5) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+'nome' varchar(20) NOT NULL UNIQUE,
+'exp' int(2) NOT NULL,
+)ENGINE=innoDB;
+
+
+/*TIMELINE*/
+DROP TABLE IF EXISTS 'timeline'
+CREATE TABLE 'timeline'(
+'id' int(5) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+'data' date NOT NULL,
+'livello' int(12) NOT NULL DEFAULT '0',
+'utente' int(5) NOT NULL REFERENCES utente ('id'),
+UNIQUE ('utente', 'livello'),
+)ENGINE=innoDB;
+
+
+/*VOTO*/
+DROP TABLE IF EXISTS 'voto'
+CREATE TABLE 'voto'(
+'votazione' int(1) NOT NULL DEFAULT '0',
+'utente' int(5) NOT NULL REFERENCES utente ('id'),
+'gioco' int(5) NOT NULL REFERENCES gioco ('id'),
+PRIMARY KEY ('utente', 'gioco'),
+)ENGINE=innoDB;
