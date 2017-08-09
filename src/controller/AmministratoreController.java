fcee79618f80454ea13dc0508d6f11540b86f7f5
D-hash <andreadasc96@gmail.com>
2017-08-09 18:41:46 +0200

    Modifiche in view e creazione GiocoController

--- src/controller/AmministratoreController.java
+++ src/controller/AmministratoreController.java
@@ -1,5 +1,33 @@
 package controller;
 
-public class AmministratoreController{
+import model.dao.concrete.*;
+import model.*;
+import java.sql.*;
 
+public class AmministratoreController extends UtenteController{
+	Utente amministratore;
+	Utente utente;
+	Gioco gioco;
+	
+	public void setModeratore(Utente amministratore){ this.amministratore= amministratore; }
+	public void setUtente(Utente utente){ this.utente = utente; }
+	public void setGioco(Gioco gioco){ this.gioco = gioco; }
+	public Utente getAmministratore(){ return this.amministratore; }
+	public Utente getUtente(){ return this.utente; }
+	public Gioco getGioco(){ return this.gioco; }
+	
+	public String insertGame(String nome, int exp) throws SQLException{
+		new GiocoDao().insertGame(new Gioco(nome, exp));
+		return "Gioco inserito con successo!";
+	}
+	
+	public String deleteGame() throws SQLException{		
+		new GiocoDao().deleteGame(gioco);
+		return "Gioco eliminato con successo!";
+	}
+	
+	public String deleteUser() throws SQLException{
+		new UtenteDao().deleteUser(utente);
+		return "Utente eliminato con successo!";
+	}
 }
--- src/controller/ModeratoreController.java
+++ src/controller/ModeratoreController.java
@@ -1,5 +1,44 @@
 package controller;
 
-public class ModeratoreController extends UtenteController{ 
+import model.dao.concrete.*;
+import model.*;
+import java.sql.*;
 
+public class ModeratoreController extends UtenteController{ 
+	// Eredita da UtenteController utente e gioco
+	Utente utente_target; // oggetto che viene passato al metodo promote o demote.
+	Recensione recensione;
+	
+	public void setUtenteTarget(Utente utente_target){ this.utente_target = utente_target; }
+	public void setRecensione(Recensione recensione){ this.recensione = recensione; }
+	public Utente getUtenteTarget(){ return this.utente_target; }
+	public Recensione getRecensione(){ return this.recensione; }
+	
+	public String promote() throws SQLException{
+		// Non serve controllare se l'utente esiste, filtraggio fatto dalla view
+		if( utente_target.getTipo().equals("amministratore") )
+			return "Promozione non riuscita, l'utente è un ammimistratore";
+
+		new UtenteDao().promoteUser(utente_target);
+		return "Promozione andata a buon fine.";
+	}
+	
+	public String demote() throws SQLException{
+		// Non serve controllare se l'utente esiste, filtraggio fatto dalla view
+		if( utente_target.getTipo().equals("amministratore") )
+			return "Retrocessione non riuscita, l'utente è un amministratore";
+
+		new UtenteDao().demoteUser(utente_target);
+		return "Retrocessione andata a buon fine.";
+	}
+	
+	public String approve() throws SQLException{
+		new UtenteDao().approveReview(recensione);
+		return "Recensione approvata.";
+	}
+	
+	public String disapprove() throws SQLException{
+		new UtenteDao().approveReview(recensione);
+		return "Recensione approvata.";
+	}
 }
