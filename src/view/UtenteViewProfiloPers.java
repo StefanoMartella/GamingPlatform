aee033ecd82cdefa53cbd717d12b1dafb50b6736
StefanoMartella <stefano.martella9614@gmail.com>
2017-08-11 02:31:08 +0200

    Bug on Moderator personal profile view  fixed

--- src/view/UtenteViewProfiloPers.java
+++ src/view/UtenteViewProfiloPers.java
@@ -81,7 +81,7 @@ public class UtenteViewProfiloPers{
 					panel_4.setVisible(false);
 					new ModeratoreView(frmPiattaformaGaming, ut);
 				}
-				if(ut.getTipo().equals("amministratore")){
+				else if(ut.getTipo().equals("amministratore")){
 					panel_4.setVisible(false);
 					new AmministratoreView(frmPiattaformaGaming, ut);
 				}
