139dfae59945150433550944f15716d0077be169
D-hash <andreadasc96@gmail.com>
2017-08-12 13:37:12 +0200

    ScrollBar lista giochi utente -da controllare

--- src/view/UtenteViewGL.java
+++ src/view/UtenteViewGL.java
@@ -2,7 +2,7 @@ package view;
 
 import controller.*;
 import model.*;
-import model.dao.concrete.*;
+
 
 import java.awt.EventQueue;
 import java.awt.Font;
@@ -19,6 +19,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.SwingConstants;
 import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.Dimension;
 
 import java.util.*;
 import java.sql.*;
@@ -36,16 +38,39 @@ public class UtenteViewGL{
 	public void initialize(){
 		
 		JPanel panel_5 = new JPanel();
-		panel_5.setBounds(0, 0, 434, 261);
+		panel_5.setBounds(0, 0, 450, 261);
 		frmPiattaformaGaming.getContentPane().add(panel_5);
 		panel_5.setLayout(null);
 		panel_5.setVisible(true);
 		
+		JPanel ps = new JPanel();
+		ps.setLayout(new GridLayout(0,1,0,20));
 		
-		JButton btnHome = new JButton("Home");
+		List<Gioco> gl = new GiocoController().listOfGames();
+		ArrayList<JButton> jl = new ArrayList<JButton>();
+		for(Gioco g: gl){
+				jl.add(new JButton(g.getNome()));
+		}
+		int i=0;
+		for(JButton j: jl){
+			j.setPreferredSize(new Dimension(40, 40));
+			ps.add(j);
+			i++;
+			j.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				panel_5.setVisible(false);
+				new GiocoView(frmPiattaformaGaming, ut, new GiocoController().findGame(j.getText()));
+		}});
+		
+		JScrollPane scroll = new JScrollPane(ps);
+		scroll.setVerticalScrollBarPolicy ( JScrollPane.VERTICAL_SCROLLBAR_ALWAYS );
+		scroll.setSize(430,200);
+		panel_5.add(scroll);
+		
+		JButton btnHome = new JButton("HOME");
 		btnHome.setBounds(335, 227, 89, 23);
 		panel_5.add(btnHome);
-		
+
 		btnHome.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(ut.getTipo().equals("moderatore")){
@@ -57,23 +82,6 @@ public class UtenteViewGL{
 					new UtenteView(frmPiattaformaGaming, ut);
 				}
 		}});
-		
-		List<Gioco> gl = new GiocoController().listOfGames();
-		ArrayList<JButton> jl = new ArrayList<JButton>();
-		for(Gioco g: gl){
-				jl.add(new JButton(g.getNome()));
-		}
-		int i=0;
-		for(JButton j: jl){
-			j.setBounds(154, 25+35*i, 160, 30);
-			panel_5.add(j);
-			i++;
-			j.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				panel_5.setVisible(false);
-				new GiocoView(frmPiattaformaGaming, ut, new GiocoController().findGame(j.getText()));
-		}});
-
 		}
 	}
 
