db4678a5566edfc52ad6224d14408eb55bad73e0
Stefano Martella <stefano.martella9614@gmail.com>
2017-08-23 00:42:03 +0200

    Update UtenteViewProfiloPers.java

--- src/view/utente/UtenteViewProfiloPers.java
+++ src/view/utente/UtenteViewProfiloPers.java
@@ -14,6 +14,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
 
 /**
 *Class which represents the view of MVC pattern of user's personal page
